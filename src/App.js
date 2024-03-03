24977bb219f008429cb483f3f0915888e5b93111
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-03-04 00:01:59 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+export function Amazon() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='container-fluid'>
+      <header className='container-fluid bg-dark'>
+     <a href=""><img src="AmazonLogo.jpg" height="55px" width="90px"/ ></a>
+      {/* <button>Location</button>
+      <input type="text" placeholder='All'/>
+      <button>button</button>
+      <button>button</button>
+      <button>button</button>
+      <button>button</button>
+      <button>button</button> */}
+      </header><br></br>
+      <div className='container-fluid h-100 bg-secondary'>jj</div>
+
     </div>
   );
 }
 
-export default App;
+// export default App;
